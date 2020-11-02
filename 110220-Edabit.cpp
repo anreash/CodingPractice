46ee870f5cfe54ae4100665cd8b9c892d21a7ac4
Anrea <69573394+anreash@users.noreply.github.com>
2020-11-02 15:25:23 +0800

    Update 110220-Edabit.cpp

--- 110220-Edabit.cpp
+++ 110220-Edabit.cpp
@@ -1,6 +1,6 @@
 // --------------------------------------------------------------
 
-/*
+/* 1. EASY level
 Create a function that returns the string "Burp" with the amount of "r's"
 determined by the input parameters of the function.
 longBurp(3) ➞ "Burrrp"
@@ -36,7 +36,7 @@ I also don't know what this means. But seems like it works.
 
 // --------------------------------------------------------------
 
-/*
+/* 2. EASY level
 Create a function which validates whether a bridge is safe to walk on (i.e. has no gaps in it to fall through).
 isSafeBridge("####") ➞ true
 isSafeBridge("## ####") ➞ false
@@ -66,16 +66,61 @@ It seems like it works like a range-for-loop, but has different parameters.
 
 // --------------------------------------------------------------
 
+/* 3. EASY level UNANSWERED
+Given a pair, return its FIRST value and its SECOND value. 
+Pair will be < int, int >.
+pairs(std::make_pair(1, 2)) ➞ { 1, 2 }
+pairs(std::make_pair(51, 21)) ➞ { 51, 21 }
+pairs(std::make_pair(512124, 215)) ➞ { 512124, 215 }
+*/
 
+//My wrong answer:
+std::vector<int> pairs(std::pair <int,int> p) {
+	pairs.pushback(p);
+	for (int x : pairs)
+		cout << x;k
+}
 
+//Most common answer:
+std::vector<int> pairs(std::pair <int,int> p){
+  return {p.first,p.second};
+}
 
+std::vector<int> pairs(std::pair <int,int> p) {
+  std::vector<int>res;
+  res.push_back(p.first);
+  res.push_back(p.second);
+  return res;
+}
+
+// Need to study this
 
 // --------------------------------------------------------------
 
+/* 4. EASY level
+Create a function that takes a string and returns the word count.
+The string will be a sentence.
+countWords("Just an example here move along") ➞ 6
+countWords("This is a test") ➞ 4
+countWords("What an easy task, right") ➞ 5
+*/
 
+int countWords(std::string str) {
+	// x is each letter in the string
+	int count{1};
+	for (char x : str){
+		if (x == ' ')
+			count++;
+	}
+	return count;
+}
 
-
-
+/*
+Better solutions:
+int countWords(std::string str) {
+  return std::count(str.begin(), str.end(), ' ') + 1;
+}
+*/
 
 // --------------------------------------------------------------
 
