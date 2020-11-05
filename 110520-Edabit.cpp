4f9d51735605ae17bdca0b20262f79b67ebd734d
Anrea <69573394+anreash@users.noreply.github.com>
2020-11-05 15:52:30 +0800

    Finished 4 Coding Challenges in Edabit

--- 110520-Edabit.cpp
+++ 110520-Edabit.cpp
@@ -160,11 +160,98 @@ It's a simplified version of my solution.
 
 // ----------------------------------------------------------------
 
-/*
+/* 4. MEDIUM level
+Create a function which simulates the game "rock, paper, scissors".
+The function takes the input of both players (rock, paper or scissors), 
+first parameter from first player, second from second player. 
+The function returns the result as such:
+"Player 1 wins"
+"Player 2 wins"
+"TIE" (if both inputs are the same)
+The rules of rock, paper, scissors, if not known:
+Both players have to say either "rock", "paper" or "scissors" at the same time.
+Rock beats scissors, paper beats rock, scissors beat paper.
+rps("rock", "paper") ➞ "Player 2 wins"
+rps("paper", "rock") ➞ "Player 1 wins"
+rps("paper", "scissors") ➞ "Player 2 wins"
+rps("scissors", "scissors") ➞ "TIE"
+rps("scissors", "paper") ➞ "Player 1 wins"
 
+NOTE: There are several ways to solve this challenge.
 */
 
+std::string rps(std::string s1, std::string s2) {
+	const std::string r = "rock";
+	const std::string p = "paper";
+	const std::string s = "scissors";
+	const std::string P2wins = "Player 2 wins";
+	const std::string P1wins = "Player 1 wins";
+	const std::string tie = "TIE";
+	
+	if (s1 == r){
+		if (s2 == r) return tie;
+		else if (s2 == p) return P2wins;
+		else if (s2 == s) return P1wins;
+	}
+	else if (s1 == p){
+		if (s2 == p) return tie;
+		else if (s2 == s) return P2wins;
+		else if (s2 == r) return P1wins;
+	}
+	else if (s1 == s){
+		if (s2 == s) return tie;
+		else if (s2 == r) return P2wins;
+		else if (s2 == p) return P1wins;
+	}
+}
 
+/*
+Better solutions:
+1. 
+	std::string rps(std::string s1, std::string s2) {
+		return s1 == s2 ? "TIE" :
+			(s1[0] == 'r' && s2[0] == 's' ? "Player 1 wins" : 
+			(s1[0] == 'p' && s2[0] == 'r' ? "Player 1 wins" : 
+			(s1[0] == 's' && s2[0] == 'p' ? "Player 1 wins" : "Player 2 wins")));
+	EXPLANATION
+	// 1/18/2020
+	// I used one statement with four ternary operations to successfully
+	// complete the challenge. My approach was to check to see if player 1
+	// can win against player 2. There are three possibilities that player 1
+	// can win against player 2, with the other six (nine total) being
+	// an equal amount of ties and loses. Therefore, if player 1 can win,
+	// then one of the bracketed ternary operators will be true. Otherwise,
+	// either the first ternary operator will be true, or the last ternary 
+	// operator is false. Here is an example with test case 3:
+	// 1. s1 == s2 | They are not the same, so move on to the next check.
+	// 2. rock == scissors | This is not true, so move on to the next check.
+	// 3. paper == rock | This is not true, so move on to the next check.
+	// 4. scissors == paper | This is not true, so player 2 wins.
+	END OF EXPLANATION
+}
+
+2. 
+std::string rps(std::string player1, std::string player2){
+    if(player1 == "rock" && player2 == "paper") return "Player 2 wins";
+    if(player1 == "paper" && player2 == "scissors") return "Player 2 wins";
+    if(player1 == "scissors" && player2 == "rock") return "Player 2 wins";
+    if(player1 == player2) return "TIE";
+    else return "Player 1 wins";
+}
+
+OBSERVATIONS:
+1. That I can return a string-sentence. No need to place them in a string variable.
+2. I should've considered the repetitive statements, which was the line of code of "TIE".
+That should've been placed in one condition.
+3. I can actually refer an index to a string by using the same with how I call an elements in a
+vector/array = s1[0] for first letter of the string.
+4. The 2 solutions above solved the problem in a way by looking at the condition-of-winning of just one player.
+If the player meets any of the condition, they win.
+Otherwise, if none meets the condition, the other player wins.
+Unless if they have the same input, it's a tie.
+5. I've learned what a ternary operation is, but still haven't explored that much.
+Need to study more about that.
+*/
 
 // ----------------------------------------------------------------
 
